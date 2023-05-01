dad251e8f1ea37a5cb578bc586e7448032948190
Betul Aksu <113881514+betulaksufo@users.noreply.github.com>
2023-05-01 18:59:32 +0300

    feat: splashView update

--- Fresh/Fresh/Features/Splash/SplashView.swift
+++ Fresh/Fresh/Features/Splash/SplashView.swift
@@ -7,14 +7,29 @@
 
 import SwiftUI
 
-struct SplashView: View {
+struct Splash: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SplashCircle(circleColor: .green, size: 100)
+            SplashCircle(circleColor: .purple, size: 20)
+            SplashCircle(circleColor: .yellow, size: 40)
+        }
     }
 }
 
-struct SplashView_Previews: PreviewProvider {
+struct Splash_Previews: PreviewProvider {
     static var previews: some View {
-        SplashView()
+        Splash()
+    }
+}
+
+private struct SplashCircle: View {
+    let circleColor: Color
+    let size: CGFloat
+
+    var body: some View {
+        Circle()
+            .fill(circleColor)
+            .frame(width: size, height: size)
     }
 }
